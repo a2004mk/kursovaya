b98f9d6b82201ea7e5efe3259f53fca114927178
Alisa <alisa2004miko@gmail.com>
2025-11-06 23:59:09 +0300

    Create menu_operations.h

---
+++ Kursach_1.0/menu_operations.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#ifndef MENU_OPERATIONS_H
+#define MENU_OPERATIONS_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <limits>
+#include "table_operations.h"
+
+using namespace std;
+
+// Прототипы функций меню
+int simpleMenu(const vector<string>& items, const string& header);
+string inputFilename(const string& prompt);
+void inputKeyColumns(int& keyColumn1, int& keyColumn2);
+bool performTableJoin(const string& table1File, const string& table2File,
+    int keyColumn1, int keyColumn2,
+    vector<TableRow>& table1, vector<TableRow>& table2,
+    vector<vector<string>>& result);
+void offerFilePreview(const string& table1File, const string& table2File);
+bool setupAndJoinTables(string& table1File, string& table2File,
+    int& keyColumn1, int& keyColumn2,
+    vector<TableRow>& table1, vector<TableRow>& table2,
+    vector<vector<string>>& result);
+bool handleOutputMenu(const string& table1File, const string& table2File,
+    vector<vector<string>>& result, bool& isSaved);
+bool handleNextMenuAfterScreen(string& table1File, string& table2File,
+    int& keyColumn1, int& keyColumn2,
+    vector<TableRow>& table1, vector<TableRow>& table2,
+    vector<vector<string>>& result, bool& isSaved);
+bool handleNextMenuAfterSave(string& table1File, string& table2File,
+    int& keyColumn1, int& keyColumn2,
+    vector<TableRow>& table1, vector<TableRow>& table2,
+    vector<vector<string>>& result, bool& isSaved);
+
+#endif
